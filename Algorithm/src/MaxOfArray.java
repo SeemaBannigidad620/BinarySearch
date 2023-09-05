c772dfa996f1525086c75767c11dd974bb7475c8
SeemaBannigidad620 <seema.bannigidad>
2023-09-05 15:04:41 +0530

    "made changes"

--- Algorithm/src/MaxOfArray.java
+++ Algorithm/src/MaxOfArray.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class MaxOfArray {
+public class MaxOfArray {
+
+    public static void main(String[] args) {
+        int[] arr = {2,6, 9,10,5};
+        int max = maxOfAnArray(arr);
+        System.out.println("Maximum element of an array " + max);
+    }
+
+    private static int maxOfAnArray(int[] arr) {
+        int max = arr[0];
+        for(int i = 0; i< arr.length; i++){
+            if(max > arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
